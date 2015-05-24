83c0a0246248800bbf3431ac79ade1530aa2fcd5
hanguofeng <hanguofeng@gmail.com>
2015-05-24 23:01:00 +0800

    add ut for redisstore

---
+++ redisstore_test.go
@@ -0,0 +1,40 @@
+// Copyright 2013 hanguofeng. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package gocaptcha
+
+import (
+	"testing"
+	"time"
+)
+
+func TestRedisStore(t *testing.T) {
+	storeConfig:=&StoreConfig{}
+	storeConfig.CaptchaConfig.LifeTime=time.Second*100
+	storeConfig.Engine="redis"
+	storeConfig.Servers= []string{"127.0.0.1:6379"}
+	store ,_:= CreateCaptchaRedisStore(storeConfig)
+
+	captcha := new(CaptchaInfo)
+	captcha.Text = "hello"
+	captcha.CreateTime = time.Now()
+
+	//test add and get
+	key := store.Add(captcha)
+	retV := store.Get(key)
+
+	if retV.Text != captcha.Text {
+		t.Errorf("not equal,retV:%s,captcha:%s", retV, captcha)
+	}
+
+	t.Logf("TestMCStore:get from redis:%s", retV)
+
+	//test del
+	store.Del(key)
+	retV = store.Get(key)
+	if nil != retV {
+		t.Errorf("not del")
+	}
+
+}
